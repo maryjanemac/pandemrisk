1ed2f6bda793a85be51b3a55f1159bfdc47deebc
Shiyu Lu <49783609+Shiyu-Lu@users.noreply.github.com>
2021-10-11 19:52:34 -0400

    Update existing_data.py

--- existing_data.py
+++ existing_data.py
@@ -9,25 +9,15 @@ import pandas as pd
 def WHOData(countryName):
     url = 'https://covid.ourworldindata.org/data/owid-covid-data.json'    
     response = urlopen(url)
-    # data = json.loads(response.read())
     df = pd.read_json(url)
     df = df.transpose()
     index = pd.Series(range(len(df)))
     df = df.set_index(index)
 
-    # print(df)
-    # print(df.columns.values)
-    # countryName = input("Enter the country name: ")
     countryData = {'location':[],'date':[],'new_cases':[],'new_deaths':[], \
             'people_vaccinated':[],'population_density':[]}
-    # countryData = pd.DataFrame(columns=['location','date','new_cases','new_deaths','icu_patients', \
-    #         'people_vaccinated','population_density','handwashing_facilities'])
-    # print(df.loc[df['location'] == countryName])
-    # print(df.loc[df['location'] == countryName]['data'][0])
-    # print(type(df.loc[df['location'] == countryName]['data'][0]))
+
     for d in df.loc[df['location'] == countryName]['data'][0]:
-        # print(d)
-        # countryData.append([countryName],d['date'],d['new_cases'],d['new_deaths'],d['icu_patients'],d['people_vaccinated'],d['population_density'],d['handwashing_facilities'])
         countryData['location'].append(countryName)
         countryData['date'].append(d['date'])
         if 'new_cases' in d.keys():
