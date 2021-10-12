2ecf39173f9bd19d91c8c5cc80a01d6d6a1fbeb5
ziyuan18 <47776767+ziyuan18@users.noreply.github.com>
2021-10-11 23:51:01 -0400

    Update fresh_data.py

--- fresh_data.py
+++ fresh_data.py
@@ -20,7 +20,7 @@ import json
 import pandas as pd
 
 
-def existingWHOData(countryName):
+def freshWHOData(countryName):
     url = 'https://covid.ourworldindata.org/data/owid-covid-data.json'    
     response = urlopen(url)
     df = pd.read_json(url)
